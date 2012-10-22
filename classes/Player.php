a9c21a2d082b46e23f506e8f4bf8ea53b8728011
micah@work <michaeleschbacher@gmail.com>
2012-10-22 13:02:31 -0500

    Updated the display to show ascii cards

--- classes/Player.php
+++ classes/Player.php
@@ -8,4 +8,25 @@ class Player
 	{
 		$this->cards[] = $card;
 	}
+
+	public function toDisplay()
+	{
+		$ret = '';
+		$p_lines = array();
+		foreach ($this->cards as $c_index => $card) {
+			$lines = $card->toDisplayLines();
+			foreach ($lines as $index => $line) {
+				if (!array_key_exists($index, $p_lines)) {
+					$p_lines[$index] = '';
+				}
+				
+				if($c_index > 0) {
+					$line = substr($line, 1);
+				}
+				$p_lines[$index] .= $line;
+//				$ret.= $line ."\n";
+			}
+		}
+		return implode("\n", $p_lines);
+	}
 }
