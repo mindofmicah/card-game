e069386a547144b495d725bcbbcb4e40c0dc2809
Micah Eschbacher <michaeleschbacher@gmail.com>
2012-10-22 16:24:26 -0500

    Added chainability and comments

--- classes/Player.php
+++ classes/Player.php
@@ -4,9 +4,17 @@ abstract class Player
 	protected $cards = array();
 	public function __construct(){}
 
+	/**
+	 * Assign a new card to the hand of a \Player
+	 *
+	 * @param $card Card the new card to add to the \Player's hand
+	 *
+	 * @return \Player
+	 */
 	public function receiveCard(Card $card)
 	{
 		$this->cards[] = $card;
+		return $this;
 	}
 
 	abstract public function toDisplay();
