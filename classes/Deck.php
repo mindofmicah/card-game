e67a4f80d925d2cfd930e35139d3f3a37d6e392e
micah@work <michaeleschbacher@gmail.com>
2012-10-22 12:38:08 -0500

    Initial commit for the Deck class

---
+++ classes/Deck.php
@@ -0,0 +1,19 @@
+<?php
+class Deck 
+{
+	protected $cards = array(), $pointer;
+	public function __construct()
+	{
+		foreach (array('clubs','hearts','spades','diamonds') as $suit) {
+			for ($i = 1; $i <= 13; $i++) {
+				$this->cards[] = new Card($i, $suit);
+			}
+		}
+		$this->pointer = 0;
+	}
+
+	public function deal()
+	{
+		return $this->cards[$this->pointer++];
+	}
+}
